2efe576fe8d8be74216dd8419d9b9a1dd4f84ccb
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-07 23:10:46 -0300

    save

--- modules/login.js
+++ modules/login.js
@@ -7,31 +7,40 @@ async function Login(name, setKickReason, deferrals, database) {
     setTimeout(() => {
         deferrals.update(`Hello ${name}. Your license is being checked.`)
 
-        let licenseIdentifier = null;
+        const stringDynamics = {
+            name: name,
+            licenseIdentifier: null
+        }
 
         for (let i = 0; i < GetNumPlayerIdentifiers(player); i++) {
             const identifier = GetPlayerIdentifier(player, i);
 
             database.debug && console.log(identifier)
             if (identifier.includes('license:')) {
-                licenseIdentifier = identifier.replace(/[^:]+:(.+)/, '$1');
+                stringDynamics.licenseIdentifier = identifier.replace(/[^:]+:(.+)/, '$1');
             }
         }
-        
+
         //pretend to be a wait
         setTimeout(async () => {
-            if (licenseIdentifier === null) {
+            if (stringDynamics.licenseIdentifier === null) {
                 deferrals.done("You are not license to game.")
             } else {
-                const user = await database.query(`SELECT id, license, whitelist FROM userData WHERE license = "${licenseIdentifier}" and whitelist = 1`)
+                const user = await database.query(`SELECT id, license, whitelist FROM userData WHERE license = "${stringDynamics.licenseIdentifier}" and whitelist = 1`)
                 
                 if (user.length) {
                     deferrals.done()
                 } else {
-                    console.log(eval('`' + login.message_register.replace(/%(.*?)%/g, '${$1}') + '`'))
+                    const deferralsMsg = login.message_register.replace(/%(\w+)%/g, function(match, p1) {
+                        if (stringDynamics.hasOwnProperty(p1)) {
+                            return stringDynamics[p1]
+                        } else {
+                            return match
+                        }
+                    })
 
-                    deferrals.done(`join in us discord ${login.discord_invite} and approve your license to whitelist: ${licenseIdentifier}`)
-                    database.query(`INSERT IGNORE INTO userData (license, whitelist, time) VALUES ("${licenseIdentifier}", 0, ${Math.floor(Date.now() / 1000)})`)
+                    deferrals.done(deferralsMsg)
+                    database.query(`INSERT IGNORE INTO userData (license, whitelist, time) VALUES ("${stringDynamics.licenseIdentifier}", 0, ${Math.floor(Date.now() / 1000)})`)
                 }
 
             }

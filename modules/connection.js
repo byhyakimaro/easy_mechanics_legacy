8b2d509ecd8d1602d044f4cfe33fd3e5ae148290
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-08 05:34:08 -0300

    save

--- modules/connection.js
+++ modules/connection.js
@@ -55,11 +55,12 @@ async function spawnManager(model, heading, idx, x, y, z, utils) {
 	const playerPed = GetPlayerPed(userId)
 
 	if (!playersInGame.includes(playerPed)) {
-		const { x, y, z } = JSON.parse((await utils.queryDb(`SELECT id, lastPos FROM userData WHERE id = '${userId}'`))[0].lastPos)
-		playersInGame.push(playerPed)
+		const queryPos = await utils.queryDb(`SELECT id, lastPos FROM userData WHERE id = '${userId}'`)
+		// const { x, y, z } = JSON.parse(queryPos[0].lastPos)
+		// playersInGame.push(playerPed)
 
-		utils.debug && console.log(playersInGame)
-		SetEntityCoords(playerPed, x, y, z, true, false, false, false)
+		utils.debug && console.log(queryPos)
+		// SetEntityCoords(playerPed, x, y, z, true, false, false, false)
 	}	else {
 		
 		SetEntityCoords(playerPed, spawn_.x, spawn_.y, spawn_.z, true, false, false, false)

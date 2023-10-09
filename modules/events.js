42baf47009c89a30af8fd4dc7684f5a785989595
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-08 23:09:22 -0300

    save

--- modules/events.js
+++ modules/events.js
@@ -1,18 +1,3 @@
-// const { spawn_ } = require("./../base/settings.json");
-
-// const playersInGame = {}
-
-// function managerEvents(utils) {
-
-//   onNet('EASY:SpawnPlayer', ({model, heading, idx, x, y, z}) => {
-//     const playerPed = GetPlayerPed(source)
-    
-//     console.log({ x, y, z })
-//     SetEntityCoords(playerPed, spawn_.x, spawn_.y, spawn_.z, true, false, false, false)
-//   })
-// }
-
-// module.exports = { managerEvents }
 const ManagerCommands = require("./commands")
 const { loginManager, spawnManager, logoutManager } = require("./connection")
 
